be3b2d5fb2eec7f974d7472606e9a8e5d45d8fc2
Redox <mail@redox.to>
2022-09-10 22:24:04 +0200

    Style

--- lua/mapvote/cl_mapvote_reopen_hint.lua
+++ lua/mapvote/cl_mapvote_reopen_hint.lua
@@ -16,22 +16,20 @@ surface.CreateFont( "MapVote_ReopenText", {
 	outline = false,
 } )
 
-hook.Add("PlayerButtonDown", "MapVote_ReopenMapvote", function(ply, button)
+hook.Add( "PlayerButtonDown", "MapVote_ReopenMapvote", function( _, button )
 	if button ~= KEY_F3 then return end
-	if not IsValid(MapVote.Panel) then return end
-
-	MapVote.Panel:SetVisible(true)
-end)
+	if not IsValid( MapVote.Panel ) then return end
+	MapVote.Panel:SetVisible( true )
+end )
 
 hook.Add( "HUDPaint", "MapVote_DrawOpenNotification", function()
-	local timeLeft = math.Round(math.Clamp(MapVote.EndTime - CurTime(), 0, math.huge))
+	local timeLeft = math.Round( math.Clamp( MapVote.EndTime - CurTime(), 0, math.huge ) )
 	local text = "Press F3 to open the mapvote menu. Changing in " .. timeLeft .. " seconds."
 
-	if not IsValid(MapVote.Panel) then return end
-	
+	if not IsValid( MapVote.Panel ) then return end
+
 	local x = ScrW() * 0.5
 	local y = ScrH() * 0.05
 	draw.SimpleText( text, "MapVote_ReopenText", x + 4, y + 4, color_black, TEXT_ALIGN_CENTER )
 	draw.SimpleText( text, "MapVote_ReopenText", x, y, color_white, TEXT_ALIGN_CENTER )
-	
 end )
