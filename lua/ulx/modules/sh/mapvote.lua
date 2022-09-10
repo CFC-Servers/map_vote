7af354da54aab3eceb64b601ed8eed50b458c1b4
Redox <mail@redox.to>
2022-09-10 22:38:05 +0200

    Style

--- lua/ulx/modules/sh/mapvote.lua
+++ lua/ulx/modules/sh/mapvote.lua
@@ -2,7 +2,7 @@ local CATEGORY_NAME = "MapVote"
 ------------------------------ VoteMap ------------------------------
 function AMB_mapvote( calling_ply, votetime, should_cancel )
 	if not should_cancel then
-		MapVote.Start(votetime, nil, nil, nil)
+		MapVote.Start( votetime )
 		ulx.fancyLogAdmin( calling_ply, "#A called a votemap!" )
 	else
 		MapVote.Cancel()
@@ -11,8 +11,8 @@ function AMB_mapvote( calling_ply, votetime, should_cancel )
 end
 
 local mapvotecmd = ulx.command( CATEGORY_NAME, "mapvote", AMB_mapvote, "!mapvote" )
-mapvotecmd:addParam{ type=ULib.cmds.NumArg, min=15, default=25, hint="time", ULib.cmds.optional, ULib.cmds.round }
-mapvotecmd:addParam{ type=ULib.cmds.BoolArg, invisible=true }
+mapvotecmd:addParam{ type = ULib.cmds.NumArg, min = 15, default = 25, hint="time", ULib.cmds.optional, ULib.cmds.round }
+mapvotecmd:addParam{ type = ULib.cmds.BoolArg, invisible = true }
 mapvotecmd:defaultAccess( ULib.ACCESS_ADMIN )
 mapvotecmd:help( "Invokes the map vote logic" )
-mapvotecmd:setOpposite( "unmapvote", {_, _, true}, "!unmapvote" )
+mapvotecmd:setOpposite( "unmapvote", { _, _, true }, "!unmapvote" )
