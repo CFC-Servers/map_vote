82824edd4779065794605f64c5a60f38dc15511c
Pierce <pierce@vulpes.dev>
2023-02-18 22:26:49 -0500

    whitespace

--- lua/mapvote/server/integrations/migrate.lua
+++ lua/mapvote/server/integrations/migrate.lua
@@ -1,23 +1,23 @@
 concommand.Add( "mapvote_migrate_playcounts", function( ply, cmd, args )
-	if IsValid(ply) and not ply:IsSuperAdmin() then return end
+	if IsValid( ply ) and not ply:IsSuperAdmin() then return end
 	local p = print
-	if IsValid(ply) then 
-		p = function(msg) ply:ChatPrint(msg) end
+	if IsValid( ply ) then 
+		p = function( msg ) ply:ChatPrint( msg ) end
 	end
 
-	local playCountsData = file.Read("mapvote/playcounts.txt" or "{}")
+	local playCountsData = file.Read( "mapvote/playcounts.txt" or "{}" )
 	if playCountsData == nil then 
-		p("could not find mapvote/playcounts.txt to migrate")
+		p( "could not find mapvote/playcounts.txt to migrate" )
 		return 
 	end
-	local playCounts = util.JSONToTable(playCountsData)
+	local playCounts = util.JSONToTable( playCountsData )
 	
-	for map, count in pairs(playCounts) do
-		map = map:sub( 1, -5 ) 
+	for map, count in pairs( playCounts ) do
+		map = map:sub( 1, -5 )
 
-		MapVote.DB.AddMap(map)
-		MapVote.DB.AddPlayCount(map, count)
+		MapVote.DB.AddMap( map )
+		MapVote.DB.AddPlayCount( map, count )
 	end
 	file.Rename( "mapvote/playcount.txt", "mapvote/playcount_backup.txt" )
-	p("MapVote migrated mapvote/playcounts.txt moving to mapvote/playcounts_backup.txt")
+	p( "MapVote migrated mapvote/playcounts.txt moving to mapvote/playcounts_backup.txt" )
 end)
