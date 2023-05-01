4570677b7b612ff8ba036dc72a94f92b5f771c82
Pierce <pierce@vulpes.dev>
2023-04-30 20:24:19 -0400

    run ttt integration in hook

--- lua/mapvote/server/integrations/ttt.lua
+++ lua/mapvote/server/integrations/ttt.lua
@@ -1,30 +1,32 @@
 if engine.ActiveGamemode() ~= "terrortown" then return end
-function CheckForMapSwitch() -- TODO see if this can be improved
-    -- Check for mapswitch
-    local rounds_left = math.max( 0, GetGlobalInt( "ttt_rounds_left", 6 ) - 1 )
-    SetGlobalInt( "ttt_rounds_left", rounds_left )
+hook.Add( "InitPostEntity", "MapVote_TTTIntegration", function()
+    function CheckForMapSwitch() -- TODO see if this can be improved
+        -- Check for mapswitch
+        local rounds_left = math.max( 0, GetGlobalInt( "ttt_rounds_left", 6 ) - 1 )
+        SetGlobalInt( "ttt_rounds_left", rounds_left )
 
-    local time_left = math.max( 0, (GetConVar( "ttt_time_limit_minutes" ):GetInt() * 60) - CurTime() )
-    local switchmap = false
-    local nextmap = string.upper( game.GetMapNext() )
+        local time_left = math.max( 0, (GetConVar( "ttt_time_limit_minutes" ):GetInt() * 60) - CurTime() )
+        local switchmap = false
+        local nextmap = string.upper( game.GetMapNext() )
 
-    if rounds_left <= 0 then
-        LANG.Msg( "limit_round", { mapname = nextmap } )
-        switchmap = true
-    elseif time_left <= 0 then
-        LANG.Msg( "limit_time", { mapname = nextmap } )
-        switchmap = true
+        if rounds_left <= 0 then
+            LANG.Msg( "limit_round", { mapname = nextmap } )
+            switchmap = true
+        elseif time_left <= 0 then
+            LANG.Msg( "limit_time", { mapname = nextmap } )
+            switchmap = true
+        end
+        if switchmap then
+            hook.Add( "TTTDelayRoundStartForVote", "MapVote_DelayRoundStart", function()
+                MuteForRestart( false )
+                return true
+            end )
+            timer.Simple( 20, function()
+                MapVote.Start()
+            end )
+        end
     end
-    if switchmap then
-        hook.Add( "TTTDelayRoundStartForVote", "MapVote_DelayRoundStart", function()
-            MuteForRestart( false )
-            return true
-        end )
-        timer.Simple( 20, function()
-            MapVote.Start()
-        end )
-    end
-end
+end )
 
 hook.Add( "MapVote_RTVStart", "MapVote_TTTStartVoteAfterRound", function()
     net.Start( "RTV_Delay" )
@@ -37,4 +39,3 @@ hook.Add( "MapVote_RTVStart", "MapVote_TTTStartVoteAfterRound", function()
     end )
     return false
 end )
-
