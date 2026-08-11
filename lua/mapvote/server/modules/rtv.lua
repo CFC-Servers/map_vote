851eaf8952cf9171f24ae5f9787de5ac7f5bb4ef
Redox <69946827+wrefgtzweve@users.noreply.github.com>
2026-08-11 22:35:10 +0200

    Make MsgN the same as printmessage

--- lua/mapvote/server/modules/rtv.lua
+++ lua/mapvote/server/modules/rtv.lua
@@ -113,7 +113,7 @@ function RTV.AddVote( ply )
 
     timer.Simple( 0, function()
         if not ply:IsValid() then return end
-        MsgN( ply:Nick() .. " has voted to change the map." )
+        MsgN( ply:Nick() .. " has voted to change the map. (" .. RTV.GetVoteCount() .. "/" .. threshold .. ")" )
         local threshold = RTV.GetThreshold()
         PrintMessage( HUD_PRINTTALK,
             ply:Nick() .. " has voted to change the map. (" .. RTV.GetVoteCount() .. "/" .. threshold .. ")" )
